7f1c97ea5f19b19d52a1c8fbc2ce8325166bb01a
test <123456>
2020-03-17 14:52:47 +0800

    第一次提交

---
+++ app/src/main/java/com/wanglijun/android_study_2020/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.wanglijun.android_study_2020
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
