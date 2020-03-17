816beaedb7ad169905b528bd414bac13f35b7c42
test <123456>
2020-03-17 21:03:40 +0800

    more 修改命名

--- app/src/main/java/com/wanglijun/android_study_2020/TopBarActivity.kt
+++ app/src/main/java/com/wanglijun/android_study_2020/TopBarActivity.kt
@@ -5,23 +5,31 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-
-class MainActivity : AppCompatActivity() {
+/**
+ * @author 王丽君
+ * Email：wanglijundev@gmail.com
+ * 描述：topBar测试类
+ * 修改历史:
+ * 修改日期         作者        版本        描述说明
+ *
+ * 创建于： 2020-03-17 21:02
+ **/
+class TopBarActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         topBar.setOnTopBarClickListener(object : TopBarView.topBarOnClickListener {
             override fun leftClick() {
-                Toast.makeText(this@MainActivity,getString(R.string.back),Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@TopBarActivity,getString(R.string.back),Toast.LENGTH_SHORT).show()
             }
 
             override fun rightClick() {
-                Toast.makeText(this@MainActivity,getString(R.string.more),Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@TopBarActivity,getString(R.string.more),Toast.LENGTH_SHORT).show()
             }
 
             override fun titleClick() {
-                Toast.makeText(this@MainActivity,getString(R.string.title),Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@TopBarActivity,getString(R.string.title),Toast.LENGTH_SHORT).show()
             }
         })
     }
