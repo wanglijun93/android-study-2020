dd6ae53e09cd4d28581772908ece2026dc7a386c
王丽君 <1402499306@qq.com>
2020-04-06 21:28:38 +0800

    viewPager实现splash添加透明度

--- wechat_app/src/main/java/com/wanglijun/wechat_app/transformer/ScaleTransformer.kt
+++ wechat_app/src/main/java/com/wanglijun/wechat_app/transformer/ScaleTransformer.kt
@@ -10,7 +10,7 @@ import androidx.viewpager.widget.ViewPager
  * @desc: 控制viewPager左右滑动时候的view大小
  */
 const val MIN_SCALE = 0.75F
-
+const val MIN_ALPHA = 0.5F
 class ScaleTransformer : ViewPager.PageTransformer {
 
     override fun transformPage(page: View, position: Float) {
@@ -18,6 +18,7 @@ class ScaleTransformer : ViewPager.PageTransformer {
             position < -1 -> {
                 page.scaleX = MIN_SCALE
                 page.scaleY = MIN_SCALE
+                page.alpha = MIN_ALPHA
             }
             position <= 1 -> {
                 //左边页面
@@ -25,16 +26,21 @@ class ScaleTransformer : ViewPager.PageTransformer {
                     val scaleA = MIN_SCALE + (1 - MIN_SCALE) * (1 + position)
                     page.scaleX = scaleA
                     page.scaleY = scaleA
+                    val alphaA = MIN_ALPHA + (1 - MIN_ALPHA) * (1 + position)
+                    page.alpha = alphaA
                 } else {
                     //右边页面
                     val scaleB = MIN_SCALE + (1 - MIN_SCALE) * (1 - position)
                     page.scaleX = scaleB
                     page.scaleY = scaleB
+                    val alphaB = MIN_ALPHA + (1 - MIN_ALPHA) * (1 - position)
+                    page.alpha = alphaB
                 }
             }
             else -> {
                 page.scaleX = MIN_SCALE
                 page.scaleY = MIN_SCALE
+                page.alpha = MIN_ALPHA
             }
         }
     }
