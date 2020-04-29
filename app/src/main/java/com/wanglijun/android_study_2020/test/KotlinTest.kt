00a408b18198ae44bdeca08540af0a6c8a9b59b6
王丽君 <1402499306@qq.com>
2020-04-29 21:45:01 +0800

    # 添加测试kotlin执行函数的方法

---
+++ app/src/main/java/com/wanglijun/android_study_2020/test/KotlinTest.kt
@@ -0,0 +1,15 @@
+package com.wanglijun.android_study_2020.test
+
+import android.widget.Toast
+
+/**
+ * @author： wlj
+ * @Date: 2020-04-24
+ * @email: wanglijundev@gmail.com
+ * @desc:
+ */
+fun getDpi(): Int = 1
+
+fun showText(text: String) {
+    println(text)
+}
