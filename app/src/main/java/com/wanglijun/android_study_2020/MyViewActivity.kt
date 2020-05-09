b6696dd3c57a1ee9ca3161c1d8283abc3ddc72e0
王丽君 <1402499306@qq.com>
2020-05-09 21:44:38 +0800

    # test代码

--- app/src/main/java/com/wanglijun/android_study_2020/MyViewActivity.kt
+++ app/src/main/java/com/wanglijun/android_study_2020/MyViewActivity.kt
@@ -2,6 +2,15 @@ package com.wanglijun.android_study_2020
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.gson.Gson
+import com.wanglijun.android_study_2020.test.GitHubService
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 /**
  * @author 王丽君
  * Email：wanglijundev@gmail.com
@@ -16,5 +25,24 @@ class MyViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_view)
+        val gson = Gson()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+
+        val gitHubService = retrofit.create(GitHubService::class.java)
+
+        gitHubService.getRepos().enqueue(object : Callback<ResponseBody> {
+            override fun onFailure(call: Call<ResponseBody>?, t: Throwable?) {
+
+            }
+
+            override fun onResponse(call: Call<ResponseBody>?, response: Response<ResponseBody>?) {
+                val reponse = Gson().toJson(response)
+                print("wlj=success $reponse")
+            }
+        })
     }
 }
